e5b16f427231c02a729e1ee1d482783900b7d3b1
Anastasiia <NMIRONENKO090@gmail.com>
2025-05-26 17:15:25 +0200

    Update style-dictionary.config.js

--- config/style-dictionary.config.js
+++ config/style-dictionary.config.js
@@ -1,1 +1,30 @@
+module.exports = {
+  source: ["tokens/figma-tokens.json"],
+  platforms: {
+    web: {
+      transformGroup: "web",
+      buildPath: "build/web/",
+      files: [{
+        destination: "variables.js",
+        format: "javascript/es6"
+      }]
+    },
+    android: {
+      transformGroup: "android",
+      buildPath: "build/android/",
+      files: [{
+        destination: "colors.xml",
+        format: "android/colors"
+      }]
+    },
+    css: {
+      transformGroup: "css",
+      buildPath: "build/css/",
+      files: [{
+        destination: "variables.css",
+        format: "css/variables"
+      }]
+    }
+  }
+};
 
